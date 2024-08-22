b48171b226c843553264a88f691aaf8148e3f9ac
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2024-08-22 16:09:56 +0530

    refactor

--- api/database.py
+++ api/database.py
@@ -4,21 +4,27 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/discsim")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://user:password@db:5432/discsim"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 Base = declarative_base()
+
 
 class UploadedFile(Base):
     __tablename__ = "uploaded_files"
-
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String, unique=True, index=True)
     content = Column(LargeBinary)
 
+
 Base.metadata.create_all(bind=engine)
+
 
 def get_engine(retries=5, delay=2):
     for attempt in range(retries):
@@ -26,11 +32,13 @@ def get_engine(retries=5, delay=2):
             engine = create_engine(DATABASE_URL)
             engine.connect()
             return engine
+
         except Exception as e:
             if attempt < retries - 1:
                 time.sleep(delay)
             else:
                 raise e
+
 
 def get_db():
     db = SessionLocal()
