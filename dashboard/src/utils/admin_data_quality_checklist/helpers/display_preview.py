28dfbb4857623be87dd8a4dfd0aa379f9948ffa5
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2024-09-04 16:17:21 +0530

    data prev

--- dashboard/src/utils/admin_data_quality_checklist/helpers/display_preview.py
+++ dashboard/src/utils/admin_data_quality_checklist/helpers/display_preview.py
@@ -5,6 +5,7 @@ def display_data_preview(uploaded_file):
     try:
         uploaded_file.seek(0)  # Reset file pointer
         df = pd.read_csv(uploaded_file)
+        st.write("Data Preview:")
         st.dataframe(df.head(), hide_index=True)
     except Exception as e:
         st.error(f"Error reading the CSV file: {str(e)}")
